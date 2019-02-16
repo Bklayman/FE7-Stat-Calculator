d709493f7a5ea5aa0d98b1230d553b3c203f0b55
Brendan Klayman <bklayma1@binghamton.edu>
2019-02-16 17:44:01 -0500

    Added DataExtractor

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
 package fe7calculator;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main{
 
     public static void main(String[] args){
 
     }    
+
+    public static List<String> getData(String fileName){
+	DataExtractor using = new DataExtractor(fileName);
+	return using.getData();
+    }
 
 }
